50a5a029336b6ebadff3d3eec61b0ffc26087c44
a.akobjanyan <a.akobjanyan@sebekon.ru>
2025-11-08 12:00:55 +0400

    favIcon added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,7 +11,9 @@ class MyDocument extends Document {
             name="viewport"
             content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, viewport-fit=cover"
           />
+          
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+          <link rel="icon" type="image/png" href="/images/favIcon.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
