30a6020310c82ec48a2af7464fbf4e44fab1b50e
a.akobjanyan <a.akobjanyan@sebekon.ru>
2025-11-08 12:56:28 +0400

    yandex code added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -17,11 +17,34 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `(function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105191265', 'ym');
+
+              ym(105191265, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `,
+            }}
+          />
         </Head>
 
         <body>
           <Main />
           <NextScript />
+          <noscript>
+            <div>
+              <img
+                src="https://mc.yandex.ru/watch/105191265"
+                style={{ position: "absolute", left: "-9999px" }}
+                alt=""
+              />
+            </div>
+          </noscript>
         </body>
       </Html>
     );
